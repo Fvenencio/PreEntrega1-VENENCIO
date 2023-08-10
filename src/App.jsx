06f44db930721b45fb59d633cc8aa622561a6501
Fabrizio Venencio <fvenencio@gmail.com>
2023-08-10 18:10:10 -0300

    se agrego formato al documento

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import Navbar from "./components/Navbar/navbar"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import Navbar from "./components/Navbar/navbar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div>
-
- 
       <Navbar />
-
-
-    
-      <ItemListContainer greeting = "Hola mundo!"/>
+      <ItemListContainer greeting="Hola mundo!" />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
